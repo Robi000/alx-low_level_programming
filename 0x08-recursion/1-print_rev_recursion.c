cf9d8268932c48856dd1c32e8655c9489d2fee01
Robi000 <88291984+Robi000@users.noreply.github.com>
2024-01-12 01:00:58 +0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,41 +1,15 @@
 #include "main.h"
 
 /**
- * _strlen_rec - returns the length of string s by calculating it
- * recursively
- * @s: the string whose length is to be calculated
- *
- * Return: @n the length of the string
- */
-int strlen_rec(char *s)
-{
-/*	printf("String is now: %s\n", s);*/
-	if (s[0] != 0)
-	{
-		return (1 + strlen_rec(&s[1]));
-	}
-	return (0);
-
-}
-
-/**
- * _print_rev_recursion - prints a string followed by a new line
- * @s: the string to be printed
- *
- * Return: void
+ * _print_rev_recursion - prints a string in reverse.
+ * @s: string
+ * Return: no return.
  */
 void _print_rev_recursion(char *s)
 {
-	if (s[])
-	int n = strlen_rec(s) - 1;
-
-	if (s[n] == 0)
+	if (*s != '\0')
 	{
-		_putchar(s[0]);
-		return;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-
-	_putchar((char) (s[n]));
-	_print_rev_recursion(&(s[n] - 1)- 1);
-
 }
